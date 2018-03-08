fde7bc0221d499f288e5dd981c1952194a014646
lowgear <lowgear1000@gmail.com>
2018-03-08 19:29:26 +0500

    practice 1 task 2, 3 done

---
+++ prct_1/task_2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace task_2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            List<int>[] graph;
+            using (var sr = new StreamReader("in.txt"))
+            {
+                n = Convert.ToInt32(sr.ReadLine());
+                graph = new List<int>[n];
+                for (int i = 0; i < n; i++)
+                    graph[i] = sr.ReadLine()
+                        .Split(' ')
+                        .Where(s => s.Length != 0)
+                        .Select(s => Convert.ToInt32(s) - 1)
+                        .Where(x => x != -1)
+                        .ToList();
+            }
+
+            var color = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                color[i] = 0;
+            }
+
+            var resn = 0;
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (color[i] != 0) continue;
+                color[i] = ++resn;
+                stack.Push(i);
+                while (stack.Count != 0)
+                {
+                    var cur = stack.Pop();
+                    foreach (var t in graph[cur])
+                    {
+                        if (color[t] != 0) continue;
+                        color[t] = color[cur];
+                        stack.Push(t);
+                    }
+                }
+            }
+
+            var res = Enumerable.Range(0, n)
+                .GroupBy(i => color[i])
+                .OrderBy(g => g.First());
+            using (var sw = new StreamWriter("out.txt"))
+            {
+                sw.WriteLine(resn);
+                foreach (var group in res)
+                {
+                    foreach (var i in group.OrderBy(i => i))
+                    {
+                        sw.Write((i + 1) + " ");
+                    }
+                    sw.WriteLine("0");
+                }
+            }
+        }
+    }
+}
---
+++ prct_1/task_3/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace task_3
+{
+    internal class Program
+    {
+        private static int INF = -32768;
+
+        private static void TopSort(int[,] matrix, int v, int[] time, int[] res)
+        {
+            for (var i = 0; i < matrix.GetLength(0); i++)
+            {
+                if (matrix[v, i] != INF)
+                    TopSort(matrix, i, time, res);
+            }
+
+            res[v] = time[0];
+            time[0]++;
+        }
+
+        public static void Main(string[] args)
+        {
+            int n;
+            int[,] matrix;
+            int from, to;
+            using (var sr = new StreamReader("in.txt"))
+            {
+                n = Convert.ToInt32(sr.ReadLine());
+                matrix = new int[n, n];
+                for (int i = 0; i < n; i++)
+                {
+                    foreach (var t in Enumerable.Range(0, n)
+                        .Zip(sr.ReadLine().Split(' ')
+                                .Where(s => s.Length != 0)
+                                .Select(s => Convert.ToInt32(s)),
+                            Tuple.Create))
+                        matrix[i, t.Item1] = t.Item2;
+                }
+
+                from = Convert.ToInt32(sr.ReadLine()) - 1;
+                to = Convert.ToInt32(sr.ReadLine()) - 1;
+            }
+
+            var r = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                r[i] = -1;
+            }
+            TopSort(matrix, from, new[]{0}, r);
+
+            var weight = new int[n];
+            var parant = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                weight[i] = INF;
+            }
+
+            weight[from] = 0;
+            parant[from] = -1;
+            foreach (var cur in Enumerable.Range(0, n)
+            .Where(i => r[i] != -1)
+            .OrderByDescending(i => r[i]))
+            {
+                foreach (var t in Enumerable.Range(0, n)
+                    .Where(v => matrix[cur, v] != INF))
+                {
+                    if (weight[t] != INF && weight[t] >= weight[cur] + matrix[cur, t]) continue;
+                    parant[t] = cur;
+                    weight[t] = weight[cur] + matrix[cur, t];
+                }
+            }
+
+            using (var sw = new StreamWriter("out.txt"))
+            {
+                if (weight[to] == INF)
+                {
+                    sw.WriteLine("N");
+                    return;
+                }
+                sw.WriteLine("Y");
+                var res = new List<int>();
+                for (int i = to; i != -1; i = parant[i])
+                    res.Add(i);
+                res.Reverse();
+                foreach (var i in res)
+                {
+                    sw.Write((i + 1) + " ");
+                }
+                sw.WriteLine();
+                sw.WriteLine(weight[to]);
+            }
+        }
+    }
+}
